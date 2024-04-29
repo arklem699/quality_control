75b06ed963831a6fbbe229edba5b02525c81a229
arklem <jrsy.tdh@yandex.ru>
2024-04-29 21:30:19 +0300

    edit quality_control/admin.py

--- quality_control/admin.py
+++ quality_control/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project', 'task')
+    search_fields = ('title', 'description')
+    list_editable = ('status',)
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'project', 'task', 'status', 'priority')
+        }),
+        ('ReadOnly', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
+
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'task', 'status', 'priority', 'created_at', 'updated_at')
+    list_filter = ('status', 'priority', 'project', 'task')
+    search_fields = ('title', 'description')
+    readonly_fields = ('created_at', 'updated_at')
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'description', 'project', 'task', 'status', 'priority')
+        }),
+        ('ReadOnly', {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
