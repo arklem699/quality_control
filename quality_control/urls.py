ffba4b94f8df9511669ba12cba1b3e47bfc10c73
arklem <jrsy.tdh@yandex.ru>
2024-04-30 21:09:47 +0300

    add forms for app quality_control

--- quality_control/urls.py
+++ quality_control/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('bugs/', views.bugs_list, name='bug_list'),
     path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_detail'),
+    path('bug/new/', views.create_bug_report, name='create_bug_report'),
     path('features/', views.features_list, name='feature_list'),
-    path('features/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail')
+    path('features/<int:feature_id>/', views.FeatureDetailView.as_view(), name='feature_detail'),
+    path('feature/new/', views.create_feature_request, name='create_feature_request'),
 ]
--- quality_control/views.py
+++ quality_control/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import DetailView
 from .models import BugReport, FeatureRequest
+from .forms import BugReportForm, FeatureRequestForm
 
 
 def index(request):
@@ -33,3 +34,25 @@ class FeatureDetailView(DetailView):
     model = FeatureRequest
     pk_url_kwarg = 'feature_id'
     template_name = 'quality_control/feature_detail.html'
+
+
+def create_bug_report(request):
+    if request.method == 'POST':
+        form = BugReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quality_control:bug_list')
+    else:
+        form = BugReportForm()
+    return render(request, 'quality_control/bug_report_form.html', {'form': form})
+
+
+def create_feature_request(request):
+    if request.method == 'POST':
+        form = FeatureRequestForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('quality_control:feature_list')
+    else:
+        form = FeatureRequestForm()
+    return render(request, 'quality_control/feature_request_form.html', {'form': form})
